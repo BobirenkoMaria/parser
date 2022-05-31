079a5a67e105cf115fabdd77c9565448836ac35f
Wayami <kittymashallllbremenno@gmail.com>
2022-05-31 16:19:35 +1000

    коммит

--- parse.py
+++ parse.py
@@ -6,7 +6,10 @@ HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
                          'Chrome/100.0.4896.127 Safari/537.36 OPR/86.0.4363.64', 'accept': '*/*'}
 
 def get_html(url, params=None):
-    r = requests.get(url, headers=HEADERS, params=params)
+    try:
+        r = requests.get(url, headers=HEADERS, params=params)
+    except requests.exceptions.ConnectionError:
+        return None
     return r
 
 
@@ -16,34 +19,31 @@ def get_url(articul, url):
 
 
 def get_content(html):
-    href_now = ''
-
     soup = BeautifulSoup(html, 'html.parser')
-
-    #try:
     items = soup.find('a', itemprop='name', href=True)
-    #except requests.exceptions.ConnectionError:
-        #items = soup.find('a', class_="product-card__figure", href=True)
 
     href_now = 'https://wasserkraft.ru/' + items.get('href')
-
-    print(href_now)
-
-    #website_parse(href_now)
+    website_parse(href_now)
 
 
 def get_website_content(html):
     info = []
 
     soup = BeautifulSoup(html, 'html.parser')
+
     title = soup.find('h1', class_="catalog-header__title title")
-    #price = soup.find('span', itemprop="price")
-    #main_photo = soup.find('a', class_="product-carousel__item zoom slick-slide slick-current slick-active", href=True)
+    main_photo = soup.find('a', class_="product-carousel__item zoom", href=True)
+    photos = soup.find_all('a', class_='product-carousel__item zoom', href=True)
+    price = soup.find('span', itemprop="price")
+
+
+
+    print(photos)
 
     info.append({
         'title': title.get_text(),
-        #'main_photo': main_photo.get('href'),
-        #'price': price.get_text()
+        'main_photo': 'https://wasserkraft.ru/' + str(main_photo.get('href')),
+        'price': price.get_text()
     })
 
     print(info)
@@ -51,19 +51,29 @@ def get_website_content(html):
 
 def parse(articuls):
     URL = ''
+
     for articul in articuls:
         URL = get_url(articul, URL)
 
-        html = get_html(URL)
-        if html.status_code == 200:
-            get_content(html.text)
-        else:
-            print('error parse')
+        html = None
+        while html == None:
+            html = get_html(URL)
+            try:
+                if html.status_code == 200:
+                    get_content(html.text)
+
+            except AttributeError:
+                print("Connection refused")
 
 
 def website_parse(url):
-    html = get_html(url)
-    if html.status_code == 200:
-        get_website_content(html.text)
-    else:
-        print('error website parse')
+    html = None
+    while html == None:
+        html = get_html(url)
+        try:
+            if html.status_code == 200:
+                #print(html.text)
+
+                get_website_content(html.text)
+        except AttributeError:
+            print("Connection refused in website_parse")
