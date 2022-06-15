cf79a718ce1bd1a4205f142be196769295fea0f1
Wayami <kittymashallllbremenno@gmail.com>
2022-06-15 17:37:45 +1000

    вроде работает но криво

--- excel_work.py
+++ excel_work.py
@@ -1,8 +1,6 @@
 import openpyxl
-from string import punctuation, whitespace
 
 articuls = []
-
 
 def set_worksheet():
     wb = openpyxl.open('TestCopy.xlsx', data_only=True)
@@ -10,21 +8,17 @@ def set_worksheet():
     sheet = wb.active
     return sheet, wb
 
+
 def work_excel(start_line, count, website):
     sheet, wb = set_worksheet()
 
     for line in range(start_line, count):
+        articuls.append({
+            (str(sheet[line][0].value)).replace(' ', '')})
 
-        if website == 1:
-            articuls.append({
-                sheet[line][0].value
-            })
-        elif website == 2:
-            articuls.append({
-                sheet[line][0].value
-            })
 
     return articuls
+
 
 def input_to_worksheet(info, start_line, count, website):
     sheet, wb = set_worksheet()
@@ -36,7 +30,9 @@ def input_to_worksheet(info, start_line, count, website):
             sheet[line][2].hyperlink = info[i]['main_photo']
             sheet[line][3].hyperlink = info[i]['photos']
             sheet[line][4].value = int(info[i]['price'].replace(' ', ''))
+            sheet[line][6].value = info[i]['dimensions']
             sheet[line][10].value = info[i]['material']
+            sheet[line][15].value = info[i]['gross weight']
         elif website == 2:
             sheet[line][1].value = info[i]['title']
             sheet[line][2].value = int(info[i]['price'].replace(' ', ''))
@@ -46,4 +42,12 @@ def input_to_worksheet(info, start_line, count, website):
 
         i+=1
 
-    wb.save('TestCopy.xlsx')
+    is_open = True
+    while is_open:
+        try:
+            wb.save('TestCopy.xlsx')
+            is_open = False
+        except PermissionError:
+            print('Закройте эксель файл и нажмите Enter')
+            input()
+            is_open = True
--- parse.py
+++ parse.py
@@ -1,11 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-import time
 
 info = []
 
 HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                          'Chrome/100.0.4896.127 Safari/537.36 OPR/86.0.4363.64', 'accept': '*/*'}
+
 
 def get_html(url, params=None):
     try:
@@ -20,47 +20,86 @@ def get_url(articul, url, website):
         url = 'https://wasserkraft.ru/search-results?query=' + str(articul)
 
     elif website == 2:
-        url = 'https://davitamebel.ru/search/?q=' + str(articul).replace("{", "").replace("}", "").replace("'","")
+        url = 'https://davitamebel.ru/search/?q=' + str(articul).replace("{", "").replace("}", "").replace("'", "")
     return url
 
 
-def get_content(html, website):
+def items_are_empty(website):
+    if website == 1:
+        info.append({
+            'title': '',
+            'main_photo': '',
+            'photos': '',
+            'price': '0',
+            'dimensions': '',
+            'material': '',
+            'gross weight': ''
+        })
+    elif website == 2:
+        info.append({
+            'title': '',
+            'price': '0',
+            'material': '',
+            'collection': '',
+            'description': ''
+        })
+
+
+def get_product_item(product_items, product):
+    for item in product_items:
+        item = item.get_text()
+        item = " ".join(item.split())
+        try:
+            if item.split()[0] == product:
+                return item
+        except IndexError:
+            pass
+    return ''
+
+
+def get_content(html, website, articul):
     soup = BeautifulSoup(html, 'html.parser')
     items = ''
     href_now = ''
 
     if website == 1:
-        items = soup.find('a', itemprop='name', href=True)
+        items = soup.find_all('a', itemprop='name', href=True)
+        articul = str(articul).replace("{", "").replace("}", "").replace("'", "")
 
-        if items != None:
-            href_now = 'https://wasserkraft.ru/' + items.get('href')
+        item_i = 0
+        needed_articul = False
+        while not needed_articul:
+            if len(items) != 0:
+                item_articul = (items[item_i].get_text()).split()[1]
 
+                if articul == item_articul:
+                    href_now = 'https://wasserkraft.ru/' + items[item_i].get('href')
+                    website_parse(href_now, website)
+                    needed_articul = True
+                else:
+                    item_i += 1
+            elif len(items)-1 == item_i:
+                items_are_empty(website)
+                needed_articul = True
+            else:
+                items_are_empty(website)
+                needed_articul = True
 
     elif website == 2:
         items = soup.find('a', target='_self', itemprop='name', href=True)
-        if items != None:
-            href_now = 'https://davitamebel.ru' + items.get('href')
+        if items is not None:
+            item = soup.find('strong')
+            item_articul = (item.get_text()).split()[1]
+            articul = str(articul).replace("{", "").replace("}", "").replace("'", "")
+            if item_articul == articul:
+                href_now = 'https://davitamebel.ru' + items.get('href')
 
-    if items != None:
-        website_parse(href_now, website)
+                website_parse(href_now, website)
+            else:
+                items_are_empty(website)
 
-    else:
-        if website == 1:
-            info.append({
-                'title': '',
-                'main_photo': '',
-                'photos': '',
-                'price': '0',
-                'material': ''
-            })
-        elif website == 2:
-            info.append({
-                'title': '',
-                'price': '0',
-                'material': '',
-                'collection': '',
-                'description': ''
-            })
+    if items == None:
+        items_are_empty(website)
 
 
 def get_website_content(html, website):
@@ -71,7 +110,11 @@ def get_website_content(html, website):
         main_photo = soup.find('a', class_="product-carousel__item zoom", href=True)
         photos = soup.find_all('a', class_='product-carousel__item zoom', href=True)
         price = soup.find('span', itemprop="price")
+
+        product_items = soup.find_all('div', class_='product__item')
+        dimensions = get_product_item(product_items, 'Размер')
         material = soup.find('div', class_='product__item not_list')
+        gross_weight = get_product_item(product_items, 'Вес')
 
 
         info.append({
@@ -79,7 +122,9 @@ def get_website_content(html, website):
             'main_photo': 'https://wasserkraft.ru/' + str(main_photo.get('href')),
             'photos': getList(photos, website),
             'price': price.get_text(),
-            'material': (str(material.get_text()).strip())[9:]
+            'dimensions': dimensions[7:-2],
+            'material': (str(material.get_text()).strip())[9:],
+            'gross weight': gross_weight[12:]
         })
     elif website == 2:
         title = soup.find('h1', itemprop="name")
@@ -103,13 +148,13 @@ def get_website_content(html, website):
             'description': description.get_text()
         })
 
-    print(info)
 
 
 def getList(tegs, website):
     str_list = []
 
     if website == 1:
+
         for teg in tegs:
                 str_list.append('https://wasserkraft.ru/' + str(teg.get('href')))
 
@@ -131,8 +176,6 @@ def getList(tegs, website):
 
 
 def parse(articuls, start_line, count, website):
-    start_time = time.time()
-
     count -= start_line
     start_line1 = start_line
 
@@ -147,7 +190,7 @@ def parse(articuls, start_line, count, website):
             html = get_html(URL)
             try:
                 if html.status_code == 200:
-                    get_content(html.text, website)
+                    get_content(html.text, website, articul)
                     start_line1 += 1
 
                     print(str(round(i*100/count)) + '%')
@@ -155,10 +198,6 @@ def parse(articuls, start_line, count, website):
 
             except AttributeError:
                 print("Connetction refused")
-
-        if i == 2:
-            end_time = time.time()
-            print(((end_time - start_time)*count)/60)
 
     return info
 
